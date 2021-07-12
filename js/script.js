6891b6f8b0f3b4611cba555a01acd2dee26fe170
Gokhan T <trgtgokhan@gmail.com>
2021-07-12 23:22:57 +0300

    Basic functionality of sketchpad has been done, needs CSS visual change.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 const container = document.querySelector(".container");
 
+const gridSize = document.getElementById('gridSize');
+const output = document.getElementById("output");
+
+output.textContent = gridSize.value;
+let sizeOfGrid = gridSize.value;
+
+gridSize.oninput = function() {
+    output.textContent = this.value;
+    sizeOfGrid = this.value;
+}
+
+const create = document.getElementById('create');
+const clean = document.getElementById('clean');
+const black = document.getElementById('blackPen');
+const rgb = document.getElementById('rgbPen');
+const borders = document.getElementById('borders');
+// const darken = document.getElementById('darken');
+
 function createDivs (size) {
     for (let i = 0 ; i < size * size ; i++) {
         const div = document.createElement('div');
+        div.className = 'cell';
+        div.classList.add(`cellNumber${i}`);
+        div.classList.add('border');
         container.appendChild(div);
     }
 }
@@ -13,21 +34,74 @@ function clear () {
     }
 }
 
-const gridSize = document.getElementById('gridSize');
-const output = document.getElementById("output");
+function cellsSetup(pen) {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            cell.classList.add(`${pen}`);
+        });
+    });
+};
 
-output.textContent = gridSize.value;
-let sizeOfGrid = gridSize.value;
-
-gridSize.oninput = function() {
-    output.textContent = this.value;
+function cleanCells() {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = 'white';
+    });
 }
 
-const create = document.getElementById('create');
+window.addEventListener('load', () => {
+    createDivs(sizeOfGrid);
+    container.style.gridTemplateColumns = `repeat(${sizeOfGrid}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${sizeOfGrid}, 1fr)`;
+    cellsSetup('black');
+});
 
 create.addEventListener('click', () => {
     clear();
     createDivs(sizeOfGrid);
     container.style.gridTemplateColumns = `repeat(${sizeOfGrid}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${sizeOfGrid}, 1fr)`;
+    cellsSetup('black');
 });
+
+clean.addEventListener('click', () => {
+    cleanCells();
+})
+
+black.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    });
+})
+
+rgb.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            let red = Math.floor(Math.random() * 255);
+            let blue = Math.floor(Math.random() * 255);
+            let green = Math.floor(Math.random() * 255);
+            cell.style.backgroundColor = 'rgb('+red+', '+blue+', '+green+')';
+        });
+    });
+})
+
+// darken.addEventListener('click', () => {
+//     let cells = document.querySelectorAll('.cell');
+//     let currentOpacity;
+//     cells.forEach((cell) => {
+//         cell.addEventListener('mouseenter', () => {
+//         });
+//     });
+// })
+
+borders.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.classList.toggle('border');
+    });
+})
